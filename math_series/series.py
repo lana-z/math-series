5787ffd61741f230fac9b4ef7dcf3ba07d03ddec
lana-z <lana@levelupeconomy.com>
2024-01-10 15:07:00 -0800

    additional tests

--- math_series/series.py
+++ math_series/series.py
@@ -1,22 +1,13 @@
-def fibonacci(num):
-    if num <= 0:
-        raise ValueError("Input must be a positive integer")
-    if num == 1:
+def fibonacci(n):
+    if n <= 0:
         return 0
-    if num == 2:
+    if n == 1:
         return 1
     
-    return fibonacci(n-1  + fibonacci(n-2)
+    return fibonacci(n-1)  + fibonacci(n-2)
 
-    # a, b = 0, 1
-    # for i in range(2, num):
-    #     a, b = b, a + b
-    # return b
-
-
-
-def lucas(n)
-    if n == 0
+def lucas(n):
+    if n == 0:
         return 2
 
     if n == 1:
@@ -24,24 +15,24 @@ def lucas(n)
 
     return lucas(n-1) + lucas(n-2)
 
-def sum_series(n, val0=0, val1=1):
+def sum_series(n, first_position=0, second_position=1):
     if n == 0:
-        return val0
+        return first_position
 
     if n == 1:
-        return val1
+        return second_position
 
-    return sum_series(n, primary=0, secondary=1):
-        if primary == 0 and secondary == 1:
-            return fibonacci(n)
-        if primary == 2 and secondary == 1:
+    return sum_series(n, first_position=0, second_position=1):
+        if first_position == 0 and second_position == 1:
+            return fibonacci(n) 
+        if first_position == 2 and second_position == 1:
             return lucas(n)
 
-        if primary == 3 and secondary == 4:
+        if first_position == 3 and second_position == 4:    
         
 # 0 1 1 2 ...
 print("fibonacci", fibonacci(3))
 
 # 2 1 3 4 ...
-print("lucas", sum_series(3, 2, 1))
+print("lucas", sum_series(3))
 
--- tests/test_series.py
+++ tests/test_series.py
@@ -19,3 +19,31 @@ def test_fibonacci_five():
     actual = fibonacci(5)
     expected = 3
     assert actual == expected
+
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_three():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+def test_lucas_five():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+
+
+
+def test_sum_series():
+    actual = sum_series(5, 3, 4)
+    expected = 11
+    assert actual == expected
